f1c4ac2bbd8fa287e62f777a6bdf726762e580ba
kamia <2920054233.com>
2025-09-24 20:52:22 +0800

    更新astro.config.mjs的site配置，适配Netlify域名

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkMermaid } from "./src/plugins/remark-mermaid.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mizuki.mysqil.com/",
+	site: "https://kamia.netlify.app",
 
 	base: "/",
 	trailingSlash: "always",
